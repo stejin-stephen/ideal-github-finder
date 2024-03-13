b785ad5b0911f4037c3cbb61c2ab0f3d797a00a1
stejin-stephen <stejinstephen@gmail.com>
2024-03-13 23:16:41 +0530

    User UI

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function Search(props: Props) {
   return (
-    <form onSubmit={props.onSubmit} className='flex items-center gap-2 shadow-md focus-within:ring-1 dark:focus-within:ring-gray-200 focus-within:ring-slate-800 p-2 rounded-lg'>
+    <form onSubmit={props.onSubmit} className='flex items-center gap-2 shadow-md focus-within:ring-1 dark:focus-within:ring-gray-200 focus-within:ring-slate-800 p-2 rounded-lg dark:bg-slate-700 bg-slate-100'>
         <section className='flex items-center w-full h-full gap-2'>
         <MdPersonSearch className='text-2xl text-blue-500' />
             <input value={props.value} onChange={props.onChange} className= "w-full h-[30px] rounded bg-inherit outline-none px-1 text-sm" type="text" placeholder="Search Github username..." />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
 import { LightAndDarkBtn } from "./components/LightAndDarkBtn";
 import Search from "./components/Search";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full p-1.5 sm:p-4 pt-10 sm:pt-12 transition-all dark:bg-slate-800">
       {/* Container */}
-      <div className="border mx-auto flex w-full max-w-[600px] flex-col gap-8 rounded p-2">
+      <div className="mx-auto flex w-full max-w-[600px] flex-col gap-8 rounded p-2">
         <section className="flex justify-between gap-3">
           <p className="text-xl font-semibold">devFinder</p>
           <LightAndDarkBtn />
@@ -14,9 +16,30 @@ export default function Home() {
               Welcome to devFinder! This is a simple app that allows you to
               search for developers on GitHub.
             </p>
-        <section>
+        <section className="flex flex-col gap-6">
           <Search />
-          <main>
+          <main className="flex w-full flex-col gap-5 rounded-lg dark:bg-slate-700 bg-slate-100 px-4 py-8 min-h-[200px]">
+            <section className="flex gap-4">
+              <Image 
+              width={200} 
+              height={200} 
+              className="h-20 w-20 rounded-full"
+              src={'https://avatars.githubusercontent.com/u/22480295'} 
+              alt="user-image" />
+              <section className="flex flex-col justify-between gap-1 transition-all sm:w-full sm:flex-row">
+                <div>
+                  <h1 className="text-2xl font-semibold">Stephen S</h1>
+                  <p className="text-sm">@StephenS</p>
+                  <Link
+                    href={"#"}
+                    className="text-sm text-blue-500 hover:underline transition-all"
+                  >
+                    View Profile
+                  </Link>
+                </div>
+                <p>Joined on July 2022</p>
+              </section>
+            </section>
           </main>
         </section>
       </div>
